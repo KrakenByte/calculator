0682a538f6440bea37ffb726a6cffe0ac0751915
Yamen Sakouhi <yamensakouhi@gmail.com>
2025-06-14 14:20:14 +0100

    Added period "." for decimal number, and improved UI

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ container.addEventListener("click", function(e) {
     if(target.tagName !== "BUTTON") return;
     
     let value = target.textContent;
-    if (io.value === "Error.") io.value = "";
+    if (io.value === "Error." || io.value === "Infinity"||  io.value === "-Infinity" || io.value === "undefined") io.value = "";
     
     if (value === "C") {
         io.value = "";
         console.log("cleared.");
     } else if (value === "=") {
         try {
-            const expression = io.value.replace(/x/g, "*").replace(/÷/g, "/");
-            io.value = Function("return " + expression)();
+            const expression = io.value
+            .replace(/×/g, "*")
+            .replace(/÷/g, "/")
+            .replace(/−/g, "-");
+            const result = Function("return " + expression)();
+            io.value = parseFloat(result.toFixed(9));
         } catch {
             io.value = "Error.";
         }
